726f56b81b1af93ddb1116c3b25f3a8492e492fe
Yiqing Zhu <yiqing_zhu2015@126.com>
2021-09-16 17:33:17 +0800

    Revert "Transport trait"
    This reverts commit eb86b0697eb54e333e5848cc8e738b5c5b9beb3d.

--- network/src/lib.rs
+++
@@ -1,9 +0,0 @@
-
-mod peer_id {
-    include!(concat!(env!("OUT_DIR"), "/peer_id.rs"));
-}
-
-pub mod error;
-
-pub mod transport;
-pub mod identity;
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
-pub use core;
+#[cfg(test)]
+
+
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
